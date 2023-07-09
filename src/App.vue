da08146fa8fe2d9ae4e8691e4f5e7732acddd791
codewait <644493354@qq.com>
2023-07-09 11:45:51 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+const clearImg = async () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({
+      target: { tabId: tabs[0].id },
+      function: () => {
+        const imgs = document.querySelectorAll('img')
+        if (imgs && imgs.length) {
+          imgs.forEach(ele => {
+            ele.remove()
+          })
+        }
+      }
+    });
+  }); 
+}
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <el-button @click="clearImg">清楚图片</el-button>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
